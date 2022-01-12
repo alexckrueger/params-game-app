4414fe5b8f6480977742eae79e958a86409b9356
Alex Krueger <alexckrueger@gmail.com>
2022-01-12 15:34:19 -0600

    adds url segment practice

--- app/controllers/number_games_controller.rb
+++ app/controllers/number_games_controller.rb
@@ -1,2 +1,32 @@
 class NumberGamesController < ApplicationController
+
+  def sum
+    num1 = params[:num1]
+    num2 = params[:num2]
+    sum = num1.to_i + num2.to_i
+    render json: {message: sum}
+  end
+
+  def guessing_numbers
+    correct_number = 36
+    number = params[:number].to_i
+
+    if number > correct_number
+      message = "That number is too high!"
+    elsif number < correct_number
+      message = "Too low :("
+    elsif number == correct_number
+      message = "Yayyyy"
+    end
+
+    render json: {message: message}
+  end
+
+  def test
+    options = params[:options]
+    additions = params["additional"]
+    render json: {message: options, message2: additions}
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   get "/name_game" => "name_games#name"
 
   get "/guess_a_number" => "name_games#number"
+
+  get "/sum_of_numbers/:num1/:num2" => "number_games#sum"
+
+  get "/guessing_numbers/:number" => "number_games#guessing_numbers"
+
+  get '/stories', to: redirect('/guess_a_number')
+
+  get "testing/*options" => "number_games#test"
+
 end
